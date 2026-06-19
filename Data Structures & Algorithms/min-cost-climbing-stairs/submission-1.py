762f0eea74124b10c32e954ecb141cfd0a3b63fd
atharvaketkar30 <127621258+atharvaketkar30@users.noreply.github.com>
2026-06-18 20:51:33 -0400

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        """
+        Repeatedly use the pattern of cost of reaching i =
+            min(cost[i-1], cost[i-2]) + cost[i]
+        """
+
+        if len(cost) <= 1:
+            return 0
+        n = len(cost)
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 0
+
+
+        for i in range(2, n+1):
+            dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]) 
+        
+        return dp[-1]
